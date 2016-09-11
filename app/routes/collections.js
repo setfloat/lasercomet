2de23138dc7556425e37774afcb9bb858a595ae3
Patrick Richardson <setfloat@gmail.com>
2016-09-10 20:17:16 -0700

    Progress on photos route api call

--- app/routes/collections.js
+++ app/routes/collections.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.post('/collections', (req, res, next) => {
+  knex('collections')
+    .orderby('id')
+    .then((rows) => {
+      console.log(rows);
+    });
+});
+
+
+module.exports = router;
--- app/routes/photos.js
+++ app/routes/photos.js
@@ -1,36 +1,49 @@
 'use strict';
 
+const apiKey = process.env.API_KEY;
+const axios = require('axios');
 const { camelizeKeys } = require('humps');
+const ev = require('express-validation');
 const express = require('express');
 const knex = require('../knex');
-const ev = require('express-validation');
 const validations = require('../validations/photos')
-const axios = require('axios');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
+
 router.get('/photos', ev(validations.get), (req, res, next) => {
-  axios({
-  method: 'get',
-  url: '',
-  data: {
-    key: process.env.API_KEY,
-    baseURL: 'https://pixabay.com/api/',
-    category,
-    webformatURL,
+  const searchQuery = req.body.searchQuery;
 
-  }
+  searchQuery = searchQuery.trim().split(' ');
+  searchQuery = searchQuery.join('+');
+
+  axios.get(`https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo`)
+    .then((res) => {
+
+      // eslint-disable-next-line no-console
+      console.log(res);
+
+    })
+    .catch((err) => next(err));
 });
 
-  knex('photos')
-  .orderBy('id')
-  .then((rows) => {
-    const photos = camelizeKeys(rows);
+  //
+  // data: {
+  //   key: process.env.API_KEY,
+  //   baseURL: 'https://pixabay.com/api/',
+  //   category,
+  //   webformatURL
+  // }
 
-    res.send(photos);
-  })
-  .catch((err) => next(err));
-});
+//   knex('photos')
+//   .orderBy('id')
+//   .then((rows) => {
+//     const photos = camelizeKeys(rows);
+//
+//     res.send(photos);
+//   })
+//   .catch((err) => next(err));
+// });
 
 module.exports = router;
